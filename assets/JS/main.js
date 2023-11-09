f04ce03f3b96fb2fc63d6597c90a91879c897fee
Claudio Schimizzi <bastaparavos@hotmail.com>
2023-11-09 00:25:47 -0300

    inicio del juego

---
+++ assets/JS/main.js
@@ -0,0 +1,33 @@
+/*
+* C = clobes => treboles
+* D = diamonds => diamantes
+* H = hearts => corazones
+* S = spades => pika
+*/
+
+
+let deck = [];
+
+
+const crearDeck = (x) => {
+
+    for( let i = 2; i <= 10; i++){
+        deck.push(i + x);
+    };
+};
+
+crearDeck("C");
+crearDeck("D");
+crearDeck("H");
+crearDeck("S");
+
+console.log(deck)
+const tipos = ["C", "D", "H", "S"];
+const crearDeckLetras = ["A", "J", "Q", "K"];
+
+for(let tipo of tipos){
+    for(let esp of crearDeckLetras){
+        deck.push(esp+tipo)
+    }
+}
+console.log(deck)
